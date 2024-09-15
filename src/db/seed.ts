761f212915443cbb64e162f5a16411677802c4af
Maicon Gabriel Friedel <maicon.friedel@gmail.com>
2024-09-14 22:12:26 -0300

    feat: created route to create a new restaurant and configured eslint

--- src/db/seed.ts
+++ src/db/seed.ts
@@ -1,42 +1,43 @@
-import { faker } from "@faker-js/faker";
-import chalk from "chalk";
-import { db } from "./";
-import { restaurants, users } from "./schema";
+/* eslint-disable drizzle/enforce-delete-with-where */
+import { faker } from '@faker-js/faker'
+import chalk from 'chalk'
+import { db } from './'
+import { restaurants, users } from './schema'
 
-console.log(chalk.blueBright("Starting database seed"));
+console.log(chalk.blueBright('Starting database seed'))
 
-await db.delete(users);
-await db.delete(restaurants);
+await db.delete(users)
+await db.delete(restaurants)
 
-console.log(chalk.yellowBright("Database reset!"));
+console.log(chalk.yellowBright('Database reset!'))
 
 await db.insert(users).values([
   {
     name: faker.person.fullName(),
     email: faker.internet.email(),
-    role: "customer",
+    role: 'customer',
   },
   {
     name: faker.person.fullName(),
     email: faker.internet.email(),
-    role: "customer",
+    role: 'customer',
   },
-]);
+])
 
-console.log(chalk.yellowBright("Created customers!"));
+console.log(chalk.yellowBright('Created customers!'))
 
 const [manager] = await db
   .insert(users)
   .values([
     {
       name: faker.person.fullName(),
-      email: "admin@admin.com",
-      role: "manager",
+      email: 'admin@admin.com',
+      role: 'manager',
     },
   ])
-  .returning({ id: users.id });
+  .returning({ id: users.id })
 
-console.log(chalk.yellowBright("Created manager!"));
+console.log(chalk.yellowBright('Created manager!'))
 
 await db.insert(restaurants).values([
   {
@@ -44,10 +45,10 @@ await db.insert(restaurants).values([
     description: faker.lorem.paragraph(),
     managerId: manager.id,
   },
-]);
+])
 
-console.log(chalk.yellowBright("Created restaurant!"));
+console.log(chalk.yellowBright('Created restaurant!'))
 
-console.log(chalk.greenBright("Database seed completed sucessfully!"));
+console.log(chalk.greenBright('Database seed completed sucessfully!'))
 
-process.exit(0);
+process.exit(0)
